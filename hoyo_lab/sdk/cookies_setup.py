0ac0c4ea0d467ad1d7b0102be5734734acfed605
Snootic <snooticonoob@hotmail.com>
2024-03-17 02:03:34 -0300

    refactor: remove manual cookie setup for cookie manager

--- hoyo_lab/sdk/cookies_setup.py
+++ hoyo_lab/sdk/cookies_setup.py
@@ -16,31 +16,14 @@ def setup_cookies(get_nickname:bool = False, nickname: str = ''):
     else:
         for user in users:
             if user.get("hoyolab_nickname") == nickname:
-                ltuid = user.get("ltuid", user.get("ltuid_v2"))
-                ltoken = user.get("ltoken", user.get("ltoken_v2"))
-                try:
-                    ltmid_v2 = user.get("ltmid_v2")
-                except:
-                    pass
-                    
-                cookies = {}
-                
-                if isinstance(ltuid,int):
-                    cookies["ltuid"] = ltuid
-                else:
-                    cookies["ltuid_v2"] = ltuid
-
-                if "v2" not in ltoken:
-                    cookies["ltoken"] = ltoken
-                else:
-                    cookies["ltoken_v2"] = ltoken
-                
-                if ltmid_v2:
-                    cookies["ltmid_v2"] = ltmid_v2
-                return cookies
+                return user
 
 def list_games():
     supported_games = abspath("hoyo_lab/games.json")
     with open(supported_games, "r") as supported_games:
         hoyo_games = json.load(supported_games)
     return hoyo_games
+
+if __name__ == "__main__":
+    setup = setup_cookies(nickname="Snootic")
+    print(setup)
